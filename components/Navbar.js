23d43daae7128e2ca496ad225418d8e5d89544d8
Lucy Toman <lucy@toman.me.uk>
2022-06-08 17:17:59 +0200

    deletes image from navbar, replaces with text

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,21 +1,12 @@
 import Link from "next/link"
 import { signOut } from "next-auth/react"
 import { useSession } from "next-auth/react"
-import Image from "next/image"
 
-const Navbar = () => {
+export default function Navbar() {
     const { data: session, status } = useSession()
 
     return (
         <div className="w-full h-12 px-10 border-b-2 bg-green-900 text-yellow-100 font-bold border-b-green-900 flex flex-row justify-between items-center">
-            {/* <div className="pt-2">
-                <Image
-                    src="/jobawesome.png"
-                    alt="job awesome logo"
-                    width="45"
-                    height="45"
-                />
-            </div> */}
             <div className="text-green-400 text-4xl">JobAwesome</div>
             <Link href="/"> Home </Link>
             <Link href="/"> My Profile* </Link>
@@ -28,5 +19,3 @@ const Navbar = () => {
         </div>
     )
 }
-
-export default Navbar
