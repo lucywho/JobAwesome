db4ee5adc0c4e7e6c5aebd3710a1caafb5b20802
Lucy Toman <lucy@toman.me.uk>
2022-06-08 17:21:21 +0200

    adds post new job function for company users

--- pages/new.js
+++ pages/new.js
@@ -1,7 +1,9 @@
 import { useState } from "react"
 import { useSession } from "next-auth/react"
+import { useRouter } from "next/router"
 
 export default function New() {
+    const router = useRouter()
     const [description, setDescription] = useState("")
     const [title, setTitle] = useState("")
     const [salary, setSalary] = useState("")
@@ -11,14 +13,33 @@ export default function New() {
     if (!session || !session.user) return null
 
     return (
-        <form>
+        <form
+            className="mt-5 "
+            onSubmit={async (e) => {
+                e.preventDefault()
+
+                await fetch("/api/job", {
+                    body: JSON.stringify({
+                        title,
+                        description,
+                        location,
+                        salary,
+                    }),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    method: "POST",
+                })
+                router.push("/dashboard")
+            }}
+        >
             <div className="flex flex-col w-1/2 mx-auto">
                 <h2 className="mt-10 mb-10 text-4xl font-bold">
                     Find your people!
                 </h2>
                 <div className=" pt-2 mt-2 mr-1">
                     <input
-                        className="border p-4 w-full text-lg font-medium bg-transparent outline-none"
+                        className="border p-2 border-green-900 rounded-md w-full text-lg"
                         placeholder="Job title"
                         required
                         onChange={(e) => setTitle(e.target.value)}
@@ -26,8 +47,8 @@ export default function New() {
                 </div>
                 <div className=" pt-2 mt-2 mr-1 ">
                     <textarea
-                        className="border p-4 w-full text-lg font-medium bg-transparent outline-none  "
-                        rows={2}
+                        className="border p-2 border-green-900 rounded-md w-full text-lg"
+                        rows={3}
                         cols={50}
                         placeholder="Job description"
                         required
@@ -36,7 +57,7 @@ export default function New() {
                 </div>
                 <div className=" pt-2 mt-2 mr-1">
                     <input
-                        className="border p-4 w-full text-lg font-medium bg-transparent outline-none  "
+                        className="border p-2 border-green-900 rounded-md w-full text-lg"
                         placeholder="Salary"
                         required
                         onChange={(e) => setSalary(e.target.value)}
@@ -44,14 +65,14 @@ export default function New() {
                 </div>
                 <div className=" pt-2 mt-2 mr-1">
                     <input
-                        className="border p-4 w-full text-lg font-medium bg-transparent outline-none color-primary "
+                        className="border p-2 border-green-900 rounded-md w-full text-lg"
                         placeholder="Location"
                         required
                         onChange={(e) => setLocation(e.target.value)}
                     />
                 </div>
                 <div className="mt-5">
-                    <button className="border float-right px-8 py-2 mt-0  font-bold rounded-full">
+                    <button className="border float-right px-8 py-2 mt-0  font-bold rounded-full bg-green-900 hover:bg-green-500 text-yellow-100 hover:text-green-900">
                         Post job
                     </button>
                 </div>
