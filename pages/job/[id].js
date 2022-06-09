e2f4c2ec252aabbe76818eb3900190bce3d31dda
Lucy Toman <lucy@toman.me.uk>
2022-06-09 13:29:25 +0200

    only displays apply now button to non-company user

--- pages/job/[id].js
+++ pages/job/[id].js
@@ -1,8 +1,10 @@
 import { getJob } from "lib/data"
 import prisma from "lib/prisma"
 import Link from "next/link"
+import { getSession } from "next-auth/react"
+import { getUser } from "lib/data"
 
-export default function Job({ job }) {
+export default function Job({ job, user }) {
     return (
         <div className="pt-10 mx-10">
             <div className="text-xl text-green-900 font-bold">{job.title}</div>
@@ -14,21 +16,30 @@ export default function Job({ job }) {
                         <span> {job.author.name}</span>
                     </Link>
                 </span>
-                <span className="bg-green-900 text-yellow-100 hover:bg-green-500 hover:text-green-900 text-sm font-bold uppercase py-2 px-3 rounded-full ml-2">
-                    <Link href={`/job/${job.id}/apply`}>apply now!</Link>
-                </span>
+                {!user.company && (
+                    <span className="bg-green-900 text-yellow-100 hover:bg-green-500 hover:text-green-900 text-sm font-bold uppercase py-2 px-3 rounded-full ml-2">
+                        <Link href={`/job/${job.id}/apply`}>apply now!</Link>
+                    </span>
+                )}
             </div>
         </div>
     )
 }
 
 export async function getServerSideProps(context) {
+    const session = await getSession(context)
+    console.log("session in job apply: ", session)
+
     let job = await getJob(context.params.id, prisma)
     job = JSON.parse(JSON.stringify(job))
+
+    let user = await getUser(session.user.id, prisma)
+    user = JSON.parse(JSON.stringify(user))
 
     return {
         props: {
             job,
+            user,
         },
     }
 }
