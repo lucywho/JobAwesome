ff7d4394926ce2e76feb932f4bbebcb4e18dbbac
Lucy Toman <lucy@toman.me.uk>
2022-07-08 13:15:19 +0200

    adds conditional email provider to nextauth

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 }
 
 module.exports = nextConfig
+
+// module.exports = {
+//     reactStrictMode: true,
+//     async redirects() {
+//         return [
+//             {
+//                 source: "/home",
+//                 destination: "/",
+//                 permanent: true,
+//             },
+//         ]
+//     },
+// }
