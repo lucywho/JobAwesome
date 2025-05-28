0318022e5ff740cfbb940f130e3d7b7079b1ae78
Lucy Toman <lucy.toman@thinslices.com>
2025-05-28 11:17:00 +0200

    fixing deployment problems: nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,47 +1,40 @@
-import NextAuth from "next-auth"
-import EmailProvider from "next-auth/providers/email"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import prisma from "lib/prisma"
+import NextAuth from 'next-auth';
+import EmailProvider from 'next-auth/providers/email';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from 'lib/prisma';
 
-let data
+let data;
 
-if (process.env.NODE_ENV === "development") {
-    data = {
-        server: process.env.EMAIL_SERVER,
-        from: process.env.EMAIL_FROM,
-    }
+if (process.env.NODE_ENV === 'development') {
+  data = {
+    server: process.env.EMAIL_SERVER,
+    from: process.env.EMAIL_FROM,
+  };
 } else {
-    data = {
-        server: {
-            host: process.env.EMAIL_SERVER_HOST,
-            port: process.env.EMAIL_SERVER_PORT,
-            auth: {
-                user: process.env.EMAIL_SERVER_USER,
-                pass: process.env.EMAIL_SERVER_PASSWORD,
-            },
-        },
-        from: process.env.EMAIL_FROM,
-    }
+  data = {
+    server: `smtp://${process.env.EMAIL_SERVER_USER}:${process.env.EMAIL_SERVER_PASSWORD}@${process.env.EMAIL_SERVER_HOST}:${process.env.EMAIL_SERVER_PORT}`,
+    from: process.env.EMAIL_FROM,
+  };
 }
 
 export default NextAuth({
-    providers: [EmailProvider(data)],
+  providers: [EmailProvider(data)],
 
-    database: process.env.DATABASE_URL,
-    secret: process.env.SECRET,
+  database: process.env.DATABASE_URL,
+  secret: process.env.SECRET,
 
-    session: {
-        jwt: true,
-        maxAge: 30 * 24 * 60 * 60, // 30 days
+  session: {
+    jwt: true,
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+
+  debug: true,
+  adapter: PrismaAdapter(prisma),
+
+  callbacks: {
+    session: async ({ session, user }) => {
+      session.user.id = user.id;
+      return Promise.resolve(session);
     },
-
-    debug: true,
-    adapter: PrismaAdapter(prisma),
-
-    callbacks: {
-        session: async ({ session, user }) => {
-            session.user.id = user.id
-            return Promise.resolve(session)
-        },
-    },
-})
+  },
+});
